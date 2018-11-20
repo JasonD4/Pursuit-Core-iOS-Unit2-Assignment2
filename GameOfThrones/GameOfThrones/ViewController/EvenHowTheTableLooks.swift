fe0e36c8ceef83a97b13c71f2abdad78f35d16b3
Jason <Jason@Pursuits-MacBook-Pro-42.local>
2018-11-20 15:42:56 -0500

    finished

---
+++ GameOfThrones/GameOfThrones/ViewController/EvenHowTheTableLooks.swift
@@ -0,0 +1,18 @@
+//
+//  EvenHowTheTableLooks.swift
+//  GameOfThrones
+//
+//  Created by Jason on 11/19/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class EvenHowTheTableLooks: UITableViewCell {
+
+    @IBOutlet weak var evenImage: UIImageView!
+    @IBOutlet weak var EvenLabel: UILabel!
+    @IBOutlet weak var EvenSeason: UILabel!
+    
+
+}
--- GameOfThrones/GameOfThrones/ViewController/ViewController.swift
+++ GameOfThrones/GameOfThrones/ViewController/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-
+// need a [1][10][2][10]
 class ViewController: UIViewController {
     @IBOutlet weak var episodeCells: UITableView!
+    private var season = [[GOTEpisode]]()
     private var gottenEpisode = [GOTEpisode](){
         // property observing
         didSet {
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     gottenEpisode = GOTEpisode.allEpisodes
+    season = [GOTEpisode.allEpisodes]
     episodeCells.dataSource = self
     episodeCells.delegate = self
     }
@@ -31,31 +33,83 @@ class ViewController: UIViewController {
         let episode = gottenEpisode[indexPath.row]
         
         DetailViewController.episodeData = episode
+        
     }
 }
-
+// good to go
 
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gottenEpisode.count
+       return gottenEpisode.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cells = tableView.dequeueReusableCell(withIdentifier: "EpisodeHolder", for: indexPath) as? HowTheTableLooks else{ fatalError()}
-        
+//        var cells = HowTheTableLooks()
+        if gottenEpisode[indexPath.row].season % 2 == 1{
         let episode = gottenEpisode[indexPath.row]
+        guard let cells = tableView.dequeueReusableCell(withIdentifier: "EpisodeHolder", for: indexPath ) as? HowTheTableLooks else{ fatalError()}
         cells.CellImage?.image = UIImage(named: episode.mediumImageID)
         cells.EpisodeName?.text = episode.name
         cells.SeasonEpisode?.text = " Season: \(episode.season) Episode: \(episode.number)"
-        
+
         
         return cells
     }
+        else {
+                let episode = gottenEpisode[indexPath.row]
+                
+                guard let cells = tableView.dequeueReusableCell(withIdentifier: "EpisodeHolderEven", for: indexPath ) as? EvenHowTheTableLooks else{ fatalError()}
+                cells.evenImage?.image = UIImage(named: episode.mediumImageID)
+                cells.EvenLabel?.text = episode.name
+                cells.EvenSeason?.text = " Season: \(episode.season) Episode: \(episode.number)"
+                
+            return cells
+
+        }
+
+    }
+  
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return gottenEpisode.count / 10
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     
     
-}
-extension ViewController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+    switch section {
+    case 0:
+    return "Season 1"
+    case 1:
+    return "Season 2"
+    case 2:
+    return "Season 3"
+    case 3:
+    return "Season 4"
+    case 4:
+    return "Season 5"
+    case 5:
+    return "Season 6"
+    case 6:
+    return "Season 7"
+    default:
+    return "Unknown"
+    }
     }
 }
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
+//extension ViewController: UITableViewDataSource{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        //this function sets up how many rows the table view has it needs to return that number
+//
+//        return GOTEpisode.allEpisodes[section].season
+//
+//}
+//}
+
+
